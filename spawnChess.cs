3a052fc880b402af30931d56ab732d73dad6f2e0
ridge2020 <60786621+ridge2020@users.noreply.github.com>
2021-03-30 14:04:03 -0500

    Add files via upload
    Skeleton for spawning pieces on a board and starting game in different scenes

---
+++ spawnChess.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnChess : MonoBehaviour
+{
+
+    public Board board;
+    public View view;
+    public GameObject piece;
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = new Board();
+        view = gameObject.AddComponent(typeof(View)) as View;
+        view.model = board;
+        view.model.newGame();
+        view.setPieces1();
+        
+    }
+}
